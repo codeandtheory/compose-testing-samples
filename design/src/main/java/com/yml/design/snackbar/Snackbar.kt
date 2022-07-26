85c580f711aa03aec0389c21c628bf8d92ff91f3
Akshay Prabhu <akshay.prabhu@ymedialabs.com>
2022-07-26 22:08:24 +0530

    1. Added signing config for release version 2. made debug implementation for showkase related tests 3. made showkase root available only in debug configuration

--- design/src/main/java/com/yml/design/snackbar/Snackbar.kt
+++ design/src/main/java/com/yml/design/snackbar/Snackbar.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
-import com.airbnb.android.showkase.annotation.ShowkaseComposable
 import com.yml.design.R
 import com.yml.design.elements.Description
 import com.yml.design.theme.Green
@@ -40,14 +39,6 @@ fun SnackMessage(
         )
     }
 }
-
-
-@Preview
-@Composable
-@ShowkaseComposable("success", "snack")
-fun SnackSuccessPreview() =
-    SnackMessage(description = stringResource(id = R.string.preview_snack_success))
-
 
 @Composable
 /*@Preview(
